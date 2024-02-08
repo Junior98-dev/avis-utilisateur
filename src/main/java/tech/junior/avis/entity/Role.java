86387c49301190b05cbe69a8f665abac6315ab5d
Junior98-dev <kounkouddorianjunior@gmail.com>
2024-02-08 16:02:53 +0100

    Creation d'un utilisateur userDetails

---
+++ src/main/java/tech/junior/avis/entity/Role.java
@@ -0,0 +1,21 @@
+package tech.junior.avis.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private TypeDeRole libelle;
+}
