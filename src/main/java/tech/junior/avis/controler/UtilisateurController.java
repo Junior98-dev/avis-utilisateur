cfdc3596781cf3b83ed021ee824859cb4048870e
Junior98-dev <kounkouddorianjunior@gmail.com>
2024-02-13 17:51:12 +0100

    Creation du controleur pour inscrire un utilisateur

--- src/main/java/tech/junior/avis/controler/UtilisateurController.java
+++ src/main/java/tech/junior/avis/controler/UtilisateurController.java
@@ -1,4 +1,26 @@
 package tech.junior.avis.controler;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tech.junior.avis.Service.UtilisateurService;
+import tech.junior.avis.entity.Utilisateur;
+
+@Slf4j
+@AllArgsConstructor
+@RestController
+@RequestMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
 public class UtilisateurController {
+
+    private UtilisateurService utilisateurService;
+
+    @PostMapping(path = "inscription")
+    public void inscription(@RequestBody Utilisateur utilisateur){
+        this.utilisateurService.inscription(utilisateur);
+        log.info("Inscription");
+    }
 }
